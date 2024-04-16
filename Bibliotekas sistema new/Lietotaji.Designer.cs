184eeb2e242b23b3da3e59af18a92a6194ed90b4
CompisPaDum <marks.logins@gmail.com>
2024-04-16 17:26:08 +0300

    Daļa no frontend un daļa no backend lietotāju formai.

--- Bibliotekas sistema new/Lietotaji.Designer.cs
+++ Bibliotekas sistema new/Lietotaji.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button6
@@ -41,21 +47,80 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Garamond", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(225, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 60);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Sistēmas lietotāji";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 148);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(752, 282);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.textBox1.Location = new System.Drawing.Point(154, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 30);
+            this.textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button1.Location = new System.Drawing.Point(588, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 32);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Meklēt";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Georgia", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button2.Location = new System.Drawing.Point(346, 436);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 31);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Veidot ierakstu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Lietotaji
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(854, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button6);
             this.Name = "Lietotaji";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Lietotaji";
+            this.Load += new System.EventHandler(this.Lietotaji_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Bibliotekas sistema new/Lietotaji.cs
+++ Bibliotekas sistema new/Lietotaji.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,37 @@ namespace Bibliotekas_sistema_new
             this.Location = new Point(400, 200);
         }
 
+        string ConnectionString;
+
         private void button6_Click(object sender, EventArgs e)
         {
             MainPage mainpage = new MainPage();
             mainpage.Show();
             this.Hide();
+        }
+
+        private void Lietotaji_Load(object sender, EventArgs e)
+        {
+            ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Marks\Downloads\Bibliotekas sistema\Bibliotekas sistema new\Database1.mdf; Integrated Security = True";
+            SqlConnection SqlCon = new SqlConnection(ConnectionString);
+            SqlCommand SqlCom = new SqlCommand();
+
+            try
+            {
+                string SqlLietotaji = "SELECT * FROM Lietotaji";
+                SqlDataAdapter DataAdapterLietotaji = new SqlDataAdapter(SqlLietotaji, SqlCon);
+                DataSet DsLietotaji = new DataSet();
+                SqlCon.Open();
+                DataAdapterLietotaji.Fill(DsLietotaji);
+                SqlCon.Close();
+
+                BindingSource BsLietotaji;
+                BsLietotaji = new BindingSource();
+                BsLietotaji.DataSource = DsLietotaji.Tables[0].DefaultView;
+                //bindingNavigator1.BindingSource = BsLietotaji;
+                dataGridView1.DataSource = BsLietotaji;
+            }
+            catch { }
         }
     }
 }
